4d342060957c45972d17a4266d990ba5d513458d
Rahul Menon <menonrahul@gmail.com>
2024-08-25 19:14:42 -0500

    set up basic options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,15 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- disable autoformat
+vim.g.autoformat = false
+
+vim.opt.autowrite = false
+
+-- disble relative line numbering
+vim.opt.relativenumber = false
+
+-- tab width 4
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
