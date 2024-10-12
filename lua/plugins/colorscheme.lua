89ca3be43a8f4131363c357e155e025d21ecc278
Rahul Menon <menonrahul02@gmail.com>
2024-10-12 17:37:59 -0500

    change colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      color_overrides = {
+        mocha = {
+          base = "#000000",
+          mantle = "#000000",
+          crust = "#000000",
+        },
+      },
+    },
+  },
+}
